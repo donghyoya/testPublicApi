3a6e17bbadac144d22e0a128b17904198ae1c22e
donghyoya <dongwooya123@gmail.com>
2024-05-29 14:54:38 +0900

    first commit

---
+++ src/test/java/test/get/openapi/OpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package test.get.openapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
